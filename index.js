d49f19bcf068284dcacd25ada87cae8813b8b9f0
thisplaceofmine <thisplaceofmine@users.noreply.github.com>
2020-04-23 19:08:18 +0900

    Heroku Ready

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const bodyParser = require('body-parser')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-require("dotenv").config();
+require('dotenv').config();
 
 mongoose.connect(process.env.mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true
+  useCreateIndex: true,
 });
 
 // mongodb connection check
-mongoose.connection.once("open", () => {
-  console.log("Mongodb connected");
+mongoose.connection.once('open', () => {
+  console.log('Mongodb connected');
 });
 
 const app = express();
@@ -21,16 +21,25 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const productsRouter = require('./routes/products')
-const invoicesRouter = require('./routes/invoices')
-const reportRouter = require('./routes/report')
+const productsRouter = require('./routes/products');
+const invoicesRouter = require('./routes/invoices');
+const reportRouter = require('./routes/report');
 
-app.use('/products', productsRouter)
-app.use('/invoices', invoicesRouter)
-app.use('/report', reportRouter)
+app.use('/products', productsRouter);
+app.use('/invoices', invoicesRouter);
+app.use('/report', reportRouter);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, function() {
-  console.log("app listening on port " + port);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+app.listen(port, function () {
+  console.log('app listening on port ' + port);
 });
