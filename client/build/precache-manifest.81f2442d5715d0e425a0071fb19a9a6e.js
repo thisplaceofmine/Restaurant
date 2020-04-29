e31c64f2b11a97258422acd973d04ce1b23afa7c
thisplaceofmine <thisplaceofmine@users.noreply.github.com>
2020-04-29 15:58:45 +0900

    Reforged

---
+++ client/build/precache-manifest.81f2442d5715d0e425a0071fb19a9a6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41792c6bf9f249050b6a9d7422b9c5a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1d14cf8d4cffb7ac60f",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "c1d14cf8d4cffb7ac60f",
+    "url": "/static/js/2.8f32d8ce.chunk.js"
+  },
+  {
+    "revision": "fd375cc9336e94d38f86f79bf9c4f2ed",
+    "url": "/static/js/2.8f32d8ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd77e268688acd5e3026",
+    "url": "/static/js/main.24d7ecbc.chunk.js"
+  },
+  {
+    "revision": "31f4014810bac943217d",
+    "url": "/static/js/runtime-main.0cbcc3fe.js"
+  }
+]);
