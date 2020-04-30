5811cd4d7d49a063e4a2beca32d7db0c7ae07554
thisplaceofmine <thisplaceofmine@users.noreply.github.com>
2020-04-30 15:12:38 +0900

    Reforged

--- client/src/action/index.js
+++ client/src/action/index.js
@@ -1,4 +1,4 @@
-import server from '../api/backend';
+import axios from 'axios';
 import { isUndefined } from 'lodash';
 
 import {
@@ -17,7 +17,7 @@ import {
 
 export const fetchProductList = () => async (dispatch) => {
   try {
-    const response = await server.get('/products/');
+    const response = await axios.get('/products/');
     dispatch({ type: FETCH_PRODUCT_LIST, payload: response.data });
   } catch (error) {
     console.log(error);
@@ -26,7 +26,7 @@ export const fetchProductList = () => async (dispatch) => {
 
 export const fetchProduct = (id) => async (dispatch) => {
   try {
-    const response = await server.get(`/products/${id}`);
+    const response = await axios.get(`/products/${id}`);
     dispatch({ type: FETCH_PRODUCT, payload: response.data });
   } catch (error) {
     console.log(error);
@@ -39,8 +39,8 @@ export const createProduct = (
   handleSuccess
 ) => async (dispatch) => {
   try {
-    await server.post('/products/add', productinfo);
-    const response = await server.get('/products/');
+    await axios.post('/products/add', productinfo);
+    const response = await axios.get('/products/');
     dispatch({ type: CREATE_PRODUCT, payload: response.data });
     handleSuccess();
   } catch (error) {
@@ -55,8 +55,8 @@ export const editProduct = (
   handleSuccess
 ) => async (dispatch) => {
   try {
-    await server.post(`/products/${id}`, productinfo);
-    const response = await server.get('/products/');
+    await axios.post(`/products/${id}`, productinfo);
+    const response = await axios.get('/products/');
     dispatch({ type: EDIT_PRODUCT, payload: response.data });
     handleSuccess();
   } catch (error) {
@@ -66,8 +66,8 @@ export const editProduct = (
 
 export const deleteProduct = (id, handleSuccess) => async (dispatch) => {
   try {
-    await server.delete(`/products/${id}`);
-    const response = await server.get('/products/');
+    await axios.delete(`/products/${id}`);
+    const response = await axios.get('/products/');
     dispatch({ type: DELETE_PRODUCT, payload: response.data });
     console.log('success');
     handleSuccess();
@@ -78,7 +78,7 @@ export const deleteProduct = (id, handleSuccess) => async (dispatch) => {
 
 export const fetchInvoiceList = () => async (dispatch) => {
   try {
-    const response = await server.get('/invoices/');
+    const response = await axios.get('/invoices/');
     dispatch({ type: FETCH_INVOICE_LIST, payload: response.data });
   } catch (error) {
     console.log(error);
@@ -87,7 +87,7 @@ export const fetchInvoiceList = () => async (dispatch) => {
 
 export const fetchInvoice = (id) => async (dispatch) => {
   try {
-    const response = await server.get(`/invoices/${id}`);
+    const response = await axios.get(`/invoices/${id}`);
     dispatch({ type: FETCH_INVOICE, payload: response.data });
   } catch (error) {
     console.log(error);
@@ -100,8 +100,8 @@ export const createInvoice = (
   handleSuccess
 ) => async (dispatch) => {
   try {
-    await server.post('/invoices/add', invoiceInfo);
-    const response = await server.get('/invoices/');
+    await axios.post('/invoices/add', invoiceInfo);
+    const response = await axios.get('/invoices/');
     dispatch({ type: CREATE_INVOICE, payload: response.data });
     handleSuccess();
   } catch (error) {
@@ -116,8 +116,8 @@ export const editInvoice = (
   handleSuccess
 ) => async (dispatch) => {
   try {
-    await server.post(`/invoices/${id}`, invoiceInfo);
-    const response = await server.get('/invoices/');
+    await axios.post(`/invoices/${id}`, invoiceInfo);
+    const response = await axios.get('/invoices/');
     dispatch({ type: EDIT_INVOICE, payload: response.data });
     handleSuccess();
   } catch (error) {
@@ -127,8 +127,8 @@ export const editInvoice = (
 
 export const deleteInvoice = (id, handleSuccess) => async (dispatch) => {
   try {
-    await server.delete(`/invoices/${id}`);
-    const response = await server.get('/invoices/');
+    await axios.delete(`/invoices/${id}`);
+    const response = await axios.get('/invoices/');
     dispatch({ type: DELETE_INVOICE, payload: response.data });
     handleSuccess();
   } catch (error) {
@@ -138,7 +138,7 @@ export const deleteInvoice = (id, handleSuccess) => async (dispatch) => {
 
 export const fetchReport = () => async (dispatch) => {
   try {
-    const response = await server.get('/report/invoices');
+    const response = await axios.get('/report/invoices');
     dispatch({ type: FETCH_REPORT, payload: response.data });
   } catch (error) {
     console.log(error);
---
+++ client/src/setupProxy.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+  app.use(
+    '/products',
+    createProxyMiddleware({ target: 'http://localhost:5000' })
+  );
+  app.use(
+    '/invoices',
+    createProxyMiddleware({ target: 'http://localhost:5000' })
+  );
+};
